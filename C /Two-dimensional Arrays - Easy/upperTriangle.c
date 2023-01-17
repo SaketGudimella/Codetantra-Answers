274e0f3381f8398ddbf51f93923a2cabd22f1617
Saket Gudimella <106355242+SaketGudimella@users.noreply.github.com>
2023-01-17 10:52:44 +0530

    Create upperTriangle.c

---
+++ C /Two-dimensional Arrays - Easy/upperTriangle.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	int o,i,j;
+	scanf("%d", &o);
+	int matrix[o][o];
+	for (i=0;i<o;i++){
+		for (j=0;j<o;j++){
+			scanf("%d", &matrix[i][j]);
+		}
+	}
+	for (i=0;i<o;i++){
+		for (j=0;j<o;j++){
+			if(i<=j){
+				printf("%d ", matrix[i][j]);
+			}else{
+				printf("0 ");
+			}
+		}
+		printf("\n");
+	}
+};
