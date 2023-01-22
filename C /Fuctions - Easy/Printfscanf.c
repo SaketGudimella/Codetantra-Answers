c6d31f5826ea2e358d383099c345bca07c7ce118
Saket Gudimella <radhasaket38@gmail.com>
2023-01-22 11:37:55 +0530

    Create Printfscanf.c

---
+++ C /Fuctions - Easy/Printfscanf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter first number: ");
+	scanf("%d", &a);
+	printf("Enter second number: ");
+	scanf("%d",&b);
+	printf("Given two numbers are : %d and %d",a,b);
+	return 0;
+}
