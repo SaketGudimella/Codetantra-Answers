e47041819f60580774b9505d2b345ad60cd9d163
Saket Gudimella <radhasaket38@gmail.com>
2023-01-22 10:42:51 +0530

    Delete Secondmaxandmin1.py

--- Python/Arrays in Python - Hard/Secondmaxandmin1.py
+++
@@ -1,6 +0,0 @@
-l=input('Enter list integers separated by space: ').split()
-for i in range(len(l)):
-	l[i]=int(l[i])
-l.remove(max(l))
-l.remove(min(l))
-print('Second Maximum and Second minimum numbers in the list: ',max(l),min(l))
